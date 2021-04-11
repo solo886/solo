dad77500cf605fd89a2dda610bc93146ceeec5e3
solo <solosamara86@gmail.com>
2021-04-11 13:15:57 +0800

    modify calculator

--- first_work/Calculator.py
+++ first_work/Calculator.py
@@ -1,11 +1,13 @@
 # @Time : 2021/4/10 13:12 
 # __author__ = 'zhangcheng'
 # coding:utf-8
+from decimal import Decimal
 
 class Calculator:
     # 相加
+
     def add(self, a, b):
-        return a+b
+        return round(a+b, 1)
 
     # 相除
     def div(self, a, b):
